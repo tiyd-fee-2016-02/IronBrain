98c874e5b1d3be452a80e21f2fe6a6c236d5bc02
Tori Hedden <tori.hedden@gmail.com>
2016-03-18 14:04:51 -0400

    add ng repeat

--- assets/js/storeCtrl.js
+++ assets/js/storeCtrl.js
@@ -1,21 +1,9 @@
-// myApp.controller('StoreController', function($scope){
-//   $scope.products = [
-//     {name: 'PowerThirst', description: 'energy, so much energy', price: '$4.20'},
-//     {name: 'Nootropics', description: 'delicioius nutrition', price: '$100'},
-//     {name: 'BicepPlane', description: 'one strong plane', price: '$150'},
-//     {name: 'IronBrain', description: 'your brain is metal now', price: '$12,000'}
-//   ];
-//
-//   $scope.storeName = "Iron Brain"
-//
-// });
-
 
 myApp.controller('StoreController', [ '$http', '$scope', function($http, $scope){
 
   $http.get('assets/json/store.json').success(function(data){
     $scope.message = "get was got"
-    $scope.product = data
+    $scope.products = data
 
 
   });
