21470060849409fc47b9dd523a34faa1d177439a
Tori Hedden <tori.hedden@gmail.com>
2016-03-18 11:43:55 -0400

    partitions out controllers into sep js files

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,20 +26,3 @@ myApp.config(function($routeProvider) {
       redirectTo: '/home'
     });
 });
-
-myApp.controller('StoreController', function($scope){
-  $scope.products = [
-    {name: 'PowerThirst', description: 'energy, so much energy', price: '$4.20'},
-    {name: 'Nootropics', description: 'delicioius nutrition', price: '$100'},
-    {name: 'BicepPlane', description: 'one strong plane', price: '$150'},
-    {name: 'IronBrain', description: 'your brain is metal now', price: '$12,000'}
-  ];
-
-  $scope.storeName = "Iron Brain"
-
-});
-
-myApp.controller('NameController', function($scope){
-  $scope.username = "Daniel"
-
-});
---
+++ assets/js/nameCtrl.js
@@ -0,0 +1,3 @@
+myApp.controller('NameController', function($scope){
+  $scope.username = "Daniel"
+});
