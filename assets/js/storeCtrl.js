7cd8fba773b2824c125f138cf375b39c2235c800
Tori Hedden <tori.hedden@gmail.com>
2016-03-18 14:37:44 -0400

    ng-repeat now adds all items to store page

--- assets/js/storeCtrl.js
+++ assets/js/storeCtrl.js
@@ -3,8 +3,7 @@ myApp.controller('StoreController', [ '$http', '$scope', function($http, $scope)
 
   $http.get('assets/json/store.json').success(function(data){
     $scope.message = "get was got"
-    $scope.products = data
-
+    $scope.products = data.items
 
   });
 }]);
