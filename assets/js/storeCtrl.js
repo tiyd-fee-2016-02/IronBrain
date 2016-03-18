96c413d83541a1cdbd044e301e58208fb97df039
Tori Hedden <tori.hedden@gmail.com>
2016-03-18 12:20:11 -0400

    print all products to page from local json

--- assets/js/storeCtrl.js
+++ assets/js/storeCtrl.js
@@ -1,23 +1,21 @@
-myApp.controller('StoreController', function($scope){
-  $scope.products = [
-    {name: 'PowerThirst', description: 'energy, so much energy', price: '$4.20'},
-    {name: 'Nootropics', description: 'delicioius nutrition', price: '$100'},
-    {name: 'BicepPlane', description: 'one strong plane', price: '$150'},
-    {name: 'IronBrain', description: 'your brain is metal now', price: '$12,000'}
-  ];
+// myApp.controller('StoreController', function($scope){
+//   $scope.products = [
+//     {name: 'PowerThirst', description: 'energy, so much energy', price: '$4.20'},
+//     {name: 'Nootropics', description: 'delicioius nutrition', price: '$100'},
+//     {name: 'BicepPlane', description: 'one strong plane', price: '$150'},
+//     {name: 'IronBrain', description: 'your brain is metal now', price: '$12,000'}
+//   ];
+//
+//   $scope.storeName = "Iron Brain"
+//
+// });
 
-  $scope.storeName = "Iron Brain"
 
+myApp.controller('StoreController', [ '$http', '$scope', function($http, $scope){
 
-  // myApp.controller('myCtrl', function($scope, $http) {
-  //   $http({
-  //       method : "GET",
-  //       url : "ViewStore.html"
-  //   }).then(function mySucces(response) {
-  //       $scope.myWelcome = response.data;
-  //   }, function myError(response) {
-  //       $scope.myWelcome = response.statusText;
-  //   });
-  // });
+  $http.get('assets/json/store.json').success(function(data){
+    $scope.message = "get was got"
+    $scope.content = data
 
-});
+  });
+}]);
