66c9ba5ccb0b59972fe05cdeb6b4bf4d183006cd
Daniel Pipkin <dpip.nc@gmail.com>
2016-03-19 18:03:52 -0400

    create page routed, search bar getting there

--- assets/js/create.js
+++ assets/js/create.js
@@ -1,4 +1,9 @@
 myApp.controller('CreateController', [ '$scope', function($scope){
 
+  $scope.master = {};
+
+  $scope.update = function(user){
+    $scope.master = angular.copy(user)
+  };
 
 }]);
