9df34ad93eb759e855b12bd71ee2648eb7ab6166
Tori Hedden <tori.hedden@gmail.com>
2016-03-21 08:50:22 -0400

    correct data type of item purchase number

--- assets/js/cartCtrl.js
+++ assets/js/cartCtrl.js
@@ -41,7 +41,7 @@ myApp.controller('CartController', ['$scope', '$http', function($scope, $http){
 
   $(".update-cart").click(function(){
     for (var i = 0; i < $scope.contents.length; i++){
-      var newQuantity = $($(".quantity")[i]).val()
+      var newQuantity = parseInt($($(".quantity")[i]).val())
       if (newQuantity <= 0){
         //delete it
         $scope.contents.splice(i, 1)
